7408b8549b6f0b76f5a9163f83565c4943cab432
pulsingcoder <yuvispace@gmail.com>
2020-08-27 17:18:06 +0530

    BeyBlade

---
+++ Assets/scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnManager : MonoBehaviourPunCallbacks
+{
+    public GameObject[] playerPrefabs;
+    public Transform[] spawnPositions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Photon CallBack Methods
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            Debug.Log("HI");
+            object playerSelectionNumber;
+            if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiplayerBeybladeARGame.PLAYER_SELECTION_NUMBER, out playerSelectionNumber))
+            {
+                int randomSpawnPoints = Random.Range(0, spawnPositions.Length - 1);
+                Vector3 instantiatePosition = spawnPositions[randomSpawnPoints].position;
+
+                PhotonNetwork.Instantiate(playerPrefabs[(int) playerSelectionNumber].name, instantiatePosition, Quaternion.identity);
+                Debug.Log("player selection number is " + (int)playerSelectionNumber);
+            }
+                
+        }
+    }
+    #endregion
+}
